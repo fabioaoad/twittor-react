7b7265a8df0bd9d945d906dedcb5d3882cc67262
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-06-12 18:24:14 -0300

    guardando datos de form de login en un estado

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button,Spinner } from "react-bootstrap"
 
 import "./SignInForm.scss"
 
 
 export default  function SignInForm() {
+  const [formData, setFormData] = useState(initialFormValue());
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Login...");
+    console.log(formData);
   };
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value})
+  }
 
     return (
       <div className="sign-in-form">
         <h2>Entrar</h2>
-        <Form onSubmit={onSubmit}>
+        <Form onSubmit={onSubmit} onChange={onChange}>
           <Form.Group>
-            <Form.Control type="email" placeholder="Correo Electrónico"></Form.Control>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Correo Electrónico"
+              defaultValue={formData.email}
+            ></Form.Control>
           </Form.Group>
           <Form.Group>
-            <Form.Control type="password" placeholder="Contraseña"></Form.Control>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+              defaultValue={formData.password}
+            ></Form.Control>
           </Form.Group>
           <Button variant="primary" type="submit">
             Iniciar sesión
@@ -27,4 +42,11 @@ export default  function SignInForm() {
         </Form>
       </div>
     );
+}
+
+function initialFormValue(){
+  return {
+    email:"",
+    password:""
+  }
 }
