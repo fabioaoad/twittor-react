afa13fa656c0833f2f995925912411af146ca4fb
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-06-27 12:51:34 -0300

    otengo id de usuaro de la URL

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Button, Spinner } from "react-bootstrap";
+import BasicLayout from "../../layout/BasicLayout";
+import { withRouter } from "react-router-dom";
+
 import "./User.scss"
 
 
-export default function User(){
+function User(props){
+  //console.log(props);
+  const { match } = props;
+  console.log(match.params.id);
   return(
-    <div>
-      <h2>User..</h2>
-    </div>
+    <BasicLayout className="user">
+      <div className="user__title">
+        <h2>Fabio Martin Aoad</h2>
+      </div>
+      <div>
+        Banner Usuario
+      </div>
+      <div>
+        Info Usuario
+      </div>
+      <div className="user__tweets">
+        Lista Tweets
+      </div>
+    </BasicLayout>
   )
-}
+  }
+
+export default withRouter(User);
