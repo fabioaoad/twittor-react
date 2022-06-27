07bc1d4e388d0b9249fe6a6338b959d8d426f54f
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-06-27 15:32:37 -0300

    Obtengo todos los datos del usuario con su ID

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Spinner } from "react-bootstrap";
 import BasicLayout from "../../layout/BasicLayout";
 import { withRouter } from "react-router-dom";
+import { toast } from "react-toastify";
+import  { getUserApi } from "../../api/user";
 
 import "./User.scss"
 
@@ -9,7 +11,23 @@ import "./User.scss"
 function User(props){
   //console.log(props);
   const { match } = props;
-  console.log(match.params.id);
+  const { params } = match
+  const [user, setUser] = useState(null);
+  //console.log(user);
+
+  useEffect(() => {
+    getUserApi(params.id).then(response => {
+      setUser(response);
+      if (!response){
+        toast.error("El usuario que has visitado no existe");
+      }
+    }).catch(() => {
+        toast.error("El usuario que has visitado no existe");
+    })
+  }, [params]);
+  
+  //console.log(match.params.id);
+
   return(
     <BasicLayout className="user">
       <div className="user__title">
