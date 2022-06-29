3eb769b2187c29169cb3ec5236dc7c871502e276
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-06-29 19:21:13 -0300

    Muestro el Avatar del usuario

--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AvatarNoFound from "../../../assets/png/avatar-no-found.png";
 import { API_HOST } from "../../../utils/constant";
 import "./BannerAvatar.scss";
 
@@ -6,12 +7,15 @@ export default function BannerAvatar(props){
   const { user } = props;
   //console.log(user);
   const bannerUrl = user?.banner ?  `${API_HOST}/obtenerBanner?id=${user.id}` : null;
-  console.log(bannerUrl);
+ // console.log(bannerUrl);
+  const avatarUrl = user?.avatar ? `${API_HOST}/obtenerAvatar?id=${user.id}` : AvatarNoFound;
   return(
     <div className="banner-avatar"
          style={{ backgroundImage:  `url('${bannerUrl}')`}}
     >
-    <div className="avatar" />
+    <div className="avatar"
+         style={{ backgroundImage:  `url('${avatarUrl}')`}}
+    />
     </div>
   );
 
