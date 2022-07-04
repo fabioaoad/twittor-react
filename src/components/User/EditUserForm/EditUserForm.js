338f8db0274a95d548500df0664d859bde9a5548
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-07-03 21:45:14 -0300

    relleno formulario con datos del usuario

--- src/components/User/EditUserForm/EditUserForm.js
+++ src/components/User/EditUserForm/EditUserForm.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Form, Button, Row, Col, FormGroup, FormControl} from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import es from "date-fns/locale/es";
 
 import "./EditUserForm.scss";
 
-export default function EditUserForm(){
-
+export default function EditUserForm(props){
+  //console.log(props);
+  const { user, setShowModal } = props;
+  const [formData, setFormData] = useState(initialValue(user));
+  //console.log(user);
   const onSubmit = e => {
   e.preventDefault();
     console.log("Editando usuario...");
+    console.log(formData);
   };
 
     return (
@@ -19,27 +23,27 @@ export default function EditUserForm(){
           <Form.Group>
             <Row>
               <Col>
-                <Form.Control type="text" placeholder="Nombre" name="nombre" />
+                <Form.Control type="text" placeholder="Nombre" name="nombre" defaultValue={formData.nombre} />
               </Col>
               <Col>
-                <Form.Control type="text" placeholder="Apellidos" name="apellidos" />
+                <Form.Control type="text" placeholder="Apellidos" name="apellidos" defaultValue={formData.apellidos}/>
               </Col>
             </Row>
           </Form.Group>
 
           <Form.Group>
-              <Form.Control as="textarea" row="3" placeholder="Agrega tu biografía" type="text" name="biografia"/>
+              <Form.Control as="textarea" row="3" placeholder="Agrega tu biografía" type="text" name="biografia" defaultValue={formData.biografia} />
           </Form.Group>
 
           <Form.Group>
-              <Form.Control type="text" placeholder="Sitio Web" name="sitioWeb" />
+              <Form.Control type="text" placeholder="Sitio Web" name="sitioWeb" defaultValue={formData.sitioWeb} />
           </Form.Group>
 
           <Form.Group>
             <DatePicker
               placeholder="Fecha de nacimiento"
             locale={es}
-              selected={new Date()}
+              selected={new Date(formData.fechaNacimiento)}
             />
           </Form.Group>
 
@@ -49,5 +53,17 @@ export default function EditUserForm(){
         </Form>
       </div>
     );
+}
+
+
+function initialValue(user){
+  return {
+    nombre: user.nombre || "",
+    apellidos: user.apellidos || "",
+    biografia: user.biografia || "",
+    ubicacion: user.ubicacion || "",
+    sitioWeb: user.sitioWeb || "",
+    fechaNacimiento: user.fechaNacimiento || ""
+  };
 }
 
