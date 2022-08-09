6f04c7461bd2da2e4a334aff39d2ab1311e51624
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-08-09 15:04:11 -0300

    Modal para crear tweets

--- src/components/LeftMenu/LeftMenu.js
+++ src/components/LeftMenu/LeftMenu.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome,faUser, faUsers, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import Logowhite from "../../assets/png/logo-white.png";
+import TweetModal from "../Modal/TweetModal";
 import { logoutApi } from "../../api/auth";
 import useAuth from "../../hooks/useAuth";
 
@@ -11,6 +12,7 @@ import "./LeftMenu.scss"
 
 export default function LeftMenu(props){
   const { setRefreshCheckLogin } = props;
+  const [showModal, setShowModal] = useState(true);
   const user = useAuth();
   //console.log(user);
   const logout = () => {
@@ -36,7 +38,9 @@ export default function LeftMenu(props){
         <FontAwesomeIcon icon={faPowerOff} /> Cerrar sesión
       </Link>
 
-      <Button>Twitear</Button>
+      <Button onClick={() => setShowModal(true)}>Twitear</Button>
+
+      <TweetModal show={showModal} setShow={setShowModal} />
     </div>
   );
 }
